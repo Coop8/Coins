61f6bc296bb3dc6a895e586a9e9274a4d2976faa
Cooper Rockwell <cooper8r8@gmail.com>
2024-07-01 17:40:22 -0400

    Initial Commit

---
+++ Coins/CoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsApp.swift
+//  Coins
+//
+//  Created by Cooper Rockwell on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinsTests/CoinsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsTests.swift
+//  CoinsTests
+//
+//  Created by Cooper Rockwell on 7/1/24.
+//
+
+import Testing
+@testable import Coins
+
+struct CoinsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
