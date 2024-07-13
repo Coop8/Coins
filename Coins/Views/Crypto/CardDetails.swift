a5eae612d6d484641e989635edcd22c528b75d10
Cooper Rockwell <cooper8r8@gmail.com>
2024-07-13 13:01:55 -0400

    new cardDetails view and linechart working. only onehour on tap rn

--- Coins/Views/Crypto/CardDetails.swift
+++ Coins/Views/Crypto/CardDetails.swift
@@ -6,13 +6,45 @@
 //
 
 import SwiftUI
+import Charts
 
 struct CardDetails: View {
+    @ObservedObject var viewModel: CoinCard.ViewModel
+    @Binding var showDetails: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            showDetails.toggle()
+        } label: {
+            Image(systemName: "xmark.circle.fill")
+        }
+        
+        if let marketData = viewModel.marketData {
+            ScrollView {
+                Chart {
+                    ForEach(marketData.prices, id: \.self) { point in
+                        let date = Date(unixTimestamp: point[0])
+                        LineMark(
+                            x: .value("Time", date),
+                            y: .value("Price", point[1])
+                        )
+                        .interpolationMethod(.catmullRom)
+                    }
+                }
+                .chartYScale(domain: viewModel.priceDomain!.min...viewModel.priceDomain!.max)
+                .frame(height: 300)
+                .padding()
+            }
+        }
+    }
+}
+
+extension Date {
+    init(unixTimestamp: Double) {
+        self.init(timeIntervalSince1970: unixTimestamp / 1000.0)
     }
 }
 
 #Preview {
-    CardDetails()
+    CardDetails(viewModel: CoinCard.ViewModel(), showDetails: .constant(true))
 }
--- Coins/Views/Crypto/CoinCard.swift
+++ Coins/Views/Crypto/CoinCard.swift
@@ -15,54 +15,52 @@ struct CoinCard: View {
     
     var body: some View {
         HStack {
-            HStack {
-                /// SwiftUI's AyncImage saves so much boilerplate
-                AsyncImage(url: URL(string: coin.image)) { phase in
-                    if let image = phase.image {
-                        image.resizable()
-                             .scaledToFit()
-                             .frame(width: 20, height: 20)
-                    } else if phase.error != nil {
-                        VStack {
-                            Image(systemName: "xmark.circle")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20, height: 20)
-                            
-                            Text("Couldn't Load Image")
-                                .font(.caption2)
-                                .multilineTextAlignment(.center)
-                        }
-                    } else {
-                        ProgressView()
+            /// SwiftUI's AyncImage saves so much boilerplate
+            AsyncImage(url: URL(string: coin.image)) { phase in
+                if let image = phase.image {
+                    image.resizable()
+                         .scaledToFit()
+                         .frame(width: 20, height: 20)
+                } else if phase.error != nil {
+                    VStack {
+                        Image(systemName: "xmark.circle")
+                            .resizable()
+                            .scaledToFit()
                             .frame(width: 20, height: 20)
+                        
+                        Text("Couldn't Load Image")
+                            .font(.caption2)
+                            .multilineTextAlignment(.center)
                     }
+                } else {
+                    ProgressView()
+                        .frame(width: 20, height: 20)
                 }
-                .padding(.trailing, 10)
+            }
+            .padding(.trailing, 10)
 
-                VStack {
-                    Text(coin.name)
-                        .font(Font.custom("MontserratRoman-Bold", size: 20))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Text("\(coin.symbol.uppercased())")
-                        .font(Font.custom("MontserratRoman-SemiBold", size: 18))
-                        .opacity(0.6)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
+            VStack {
+                Text(coin.name)
+                    .font(Font.custom("MontserratRoman-Bold", size: 20))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("\(coin.symbol.uppercased())")
+                    .font(Font.custom("MontserratRoman-SemiBold", size: 18))
+                    .opacity(0.6)
+                    .frame(maxWidth: .infinity, alignment: .leading)
             }
             
             Spacer()
             
-            VStack {
+            VStack(alignment: .trailing) {
                 Text("$\(coin.current_price, specifier: "%0.2f")")
                     .font(Font.custom("MontserratRoman-Medium", size: 16))
                 
                 HStack(spacing: 2) {
-                    Text(coin.price_change_24h.isLess(than: 0) ? "-" : "+")
+                    Text(coin.price_change_percentage_24h.isLess(than: 0) ? "-" : "+")
                         .font(Font.custom("MontserratRoman-Medium", size: 14))
 
-                    Text("\(coin.price_change_24h.magnitude, specifier: "%0.2f")")
+                    Text("\(coin.price_change_percentage_24h.magnitude, specifier: "%0.2f")%")
                         .font(Font.custom("MontserratRoman-Medium", size: 14))
                 }                        
                 .foregroundStyle(coin.price_change_24h.isLess(than: 0) ? .red : .green)
@@ -72,61 +70,11 @@ struct CoinCard: View {
         .frame(maxWidth: .infinity, maxHeight: 75)
         .onTapGesture {
             showDetails.toggle()
+            viewModel.fetchHistoricalData(for: coin.id, timeRange: .oneHour)
         }
         .fullScreenCover(isPresented: $showDetails) {
-            Button {
-                showDetails.toggle()
-            } label: {
-                Image(systemName: "xmark.circle.fill")
-            }
-            .task {
-                viewModel.fetchHistoricalData(for: coin.id, timeRange: .twentyFourHours)
-            }
-
-//            HStack {
-//                Button {
-//                } label: {
-//                    Text("Day")
-//                }
-//                
-//                Button {
-//                } label: {
-//                    Text("Month")
-//                }
-//                
-//                Button {
-//                } label: {
-//                    Text("Year")
-//                }
-//            }
-            
-            if let marketData = viewModel.marketData {
-                ScrollView {
-                    ForEach(marketData.prices, id: \.self) { price in
-                        HStack {
-                            Text("Timestamp: \(Date(unixTimestamp: price[0]), formatter: DateFormatter.customFormatter)")
-                            Spacer()
-                            Text("Price: \(price[1], specifier: "%.2f")")
-                        }
-                    }
-                }
-            }
+            CardDetails(viewModel: viewModel, showDetails: $showDetails)
         }
-    }
-}
-
-extension DateFormatter {
-    static let customFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .short
-        return formatter
-    }()
-}
-
-extension Date {
-    init(unixTimestamp: Double) {
-        self.init(timeIntervalSince1970: unixTimestamp / 1000.0)
     }
 }
 
